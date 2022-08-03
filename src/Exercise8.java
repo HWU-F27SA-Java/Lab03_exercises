63080e2552b220353b796e68ead333315b360b1a
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:21:11 +0100

    Create Exercise8.java

---
+++ src/Exercise8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Week 3, Exercise 8.
+ * @author INSERT YOUR NAME HERE
+ */
+public class Exercise8 {
+
+	public static void main(String[] args) {
+		/* First, copy across the code in the main method
+		   from your solution to Exercise 7. */
+		
+	}
+
+}
